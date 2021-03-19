5e04acbff35a906bf3f7c50bd005bb3b6dfc110a
ColeyDohmen <coley.dawn12@gmail.com>
2021-03-19 14:16:53 -0600

    server up

--- checkpoint7buglog.client/src/AuthConfig.js
+++ checkpoint7buglog.client/src/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-v48qe94c.us.auth0.com'
+export const clientId = 'ELM4CqHSbanmUBu9El6JzRj3bUpZvC4a'
+export const audience = 'https://buglog'
